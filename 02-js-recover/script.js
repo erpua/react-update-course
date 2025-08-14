e96bd2ff88693aa00075ddb0d76db37c8cc7b20f
Ievgen <zhe.tovtin@gmail.com>
2025-08-13 21:06:29 -0600

    added destructuring

--- 02-js-recover/script.js
+++ 02-js-recover/script.js
@@ -142,3 +142,14 @@ function getBooks() {
 function getBook(id) {
   return data.find((d) => d.id === id);
 }
+
+// Destructuring
+
+const book = getBook(3);
+
+const title = book.title;
+const author = book.author;
+
+//const { title, author, pages, publicationDate, genres, hasMovieAdaptation } = book;
+
+console.log(author, title, genres);
